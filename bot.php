f3a3974801b9bdfab889b32f07562ea5a80d79b0
geraldforkin <88085663+geraldforkin@users.noreply.github.com>
2021-07-28 13:57:59 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,10 @@
 header("Content-Type: image/png");
-$im = @imagecreate(110, 20)
+$im = @imagecreatefrompng("https://raw.githubusercontent.com/geraldforkin/bbb/main/pr.png")
     or die("Невозможно создать поток изображения");
+
+$red = imageColorAllocate($image, 184, 39, 42);
+$black = imageColorAllocate($image, 51, 51, 51);
+
 $background_color = imagecolorallocate($im, 0, 0, 0);
 $text_color = imagecolorallocate($im, 233, 14, 91);
 imagestring($im, 1, 5, 5,  "A Simple Text String", $text_color);
