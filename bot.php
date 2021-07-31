1edaaa2b0d480425aadc8ad336374a472d1fb09c
geraldforkin <88085663+geraldforkin@users.noreply.github.com>
2021-07-31 14:57:15 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,5 +10,31 @@ $bot_config = $bot_config[0];
 
 
 
+$bot_chanels = file_get_contents('./chanels.json');
+$bot_chanels = json_decode($bot_chanels); 
+$bot_chanels = $bot_chanels[0];
+ 
+function curs($sum,$from,$to){
+    $curses = file_get_contents('https://www.cbr-xml-daily.ru/daily_json.js');
+    $curses = json_decode($curses);
+    $from_perone = $curses->Valute->{$from}->Value/$curses->Valute->{$from}->Nominal;
+    $from_perall = $from_perone*$sum;
+    
+    if($to=='RUB'||!$to||!$curses->Valute->{$to}){
+        $go = floor($from_perall).' руб.';
+    }else{
+        $to_perone = $curses->Valute->{$to}->Value/$curses->Valute->{$to}->Nominal;
+
+        $cur = $to;
+        if($to=='USD'){ $cur = '$';}
+        if($to=='EUR'){ $cur = '€';}
+        if($to=='UAH'){ $cur = 'грн.';} 
+
+        $go = floor($from_perall/$to_perone).' '.$cur;
+    }
+    return $go;
+}
+ 
+
 
 
