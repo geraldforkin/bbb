de8b361970d09ef6290d08484136ebd1fc81766a
geraldforkin <88085663+geraldforkin@users.noreply.github.com>
2021-08-04 17:49:26 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,4 +1,8 @@
- 
+ ini_set('display_errors', 1); 
+error_reporting(E_ALL);  
+require_once('./api/Connector.php');
+$con = new Connector();
+
 $bot_config = file_get_contents('https://raw.githubusercontent.com/geraldforkin/bbb/main/bot_config.json');
 $bot_config = json_decode($bot_config); 
 $bot_config = $bot_config[0];
