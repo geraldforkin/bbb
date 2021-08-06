dd1a543ed917cbf86fd45fa72d6f414fbc1b950d
geraldforkin <88085663+geraldforkin@users.noreply.github.com>
2021-08-06 14:53:37 +0300

    Update check.php

--- check.php
+++ check.php
@@ -10,9 +10,9 @@ $font_avenir = './avenirnextfont.ttf';
 $rand = array(175.16,145.11,115.09,143.02);
 $rand = $rand[array_rand($rand)];
  
-imagefttext($image, 40, 0, 29, 320, $red, $font_file, '-'.$_GET['s'].' '.$_GET['c']);
+imagefttext($image, 40, 0, 29, 320, $red, $font_file, '-'.$s.' '.$c);
 imagefttext($image, 25, 0, 29, 420, $black, $font_avenir, date('d-m-Y'));
-imagefttext($image, 28, 0, 29, 830, $black, $font_avenir, ($_GET['s']+$rand).' '.$_GET['c']);
+imagefttext($image, 28, 0, 29, 830, $black, $font_avenir, ($s+$rand).' '.$c);
 imagefttext($image, 25, 0, 29, 1020, $black, $font_avenir, date('d-m-Y'));
  
 imagepng($image);
